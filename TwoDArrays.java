df3685fe53887bd626b3ae64c4d8b4197228df88
darwin <95459719+darwinh03@users.noreply.github.com>
2021-12-16 13:44:26 -0800

    Add files via upload

---
+++ TwoDArrays.java
@@ -0,0 +1,35 @@
+/*
+Darwin Hanson
+12/16/2021
+   
+Two-D Arrays demo will show declaring a 2-dimensional
+and accessing its elements.
+*/
+import java.util.Arrays;
+
+public class TwoDArrays {
+   public static void main(String[] args) {
+      // declare a 2D array using "new" keyword
+      int rows = 2;
+      int cols = 3;
+      int[][] first = new int[rows][cols];
+      
+      // assign some values to the 2D array
+      first[0][0] = 1;
+      first[0][1] = 2;
+      first[0][2] = 3;
+      first[1][0] = 4;
+      first[1][1] = 5;
+      
+      // print out the array using deepToString() method
+      System.out.println(Arrays.deepToString(first));
+      
+      // create a 2D array using the { } initializer list syntax
+      int[][] second = { { 1, 3, 5, 7 },
+                         { 2, 4, 6, 8 },
+                         { 0, 1, 2, 3 } };
+
+      // print out the array using deepToString() method
+      System.out.println(Arrays.deepToString(second));
+   }
+}
