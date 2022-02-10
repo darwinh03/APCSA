96de937a99da9d6772175ac2309f4eafb2ab5fd6
darwin <95459719+darwinh03@users.noreply.github.com>
2022-02-10 13:17:34 -0800

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -11,10 +11,79 @@ We will create Pet instances in the main() method of the AnimalClinic class
 public class Pet {
    // declare private fields for the name, age, weight, and type of animal
    private String name;
-   private int age;
-   private int weight;
-   private String typeOfAnimal;
-   // write 4 constructors, accessor (get) methods, mutator (set) methods, and 
+   private int newAge;
+   private double newWeight;
+   private String newTypeOfAnimal;
+   // write 2 constructors, accessor (get) methods, mutator (set) methods, and 
    // the toString() method. Use good commenting.
+   
+   // CONSTRUCTORS
+   // first constructor- takes 4 parameters
+   public Pet(String name, int age, double weight, String typeOfAnimal) {
+      setName(name);
+      setAge(age);
+      setWeight(weight); 
+      setAnimal(typeOfAnimal);
+   }
+   
+   // second constructor- takes 0 parameters and sets to default values
+   public Pet() {
+      this("Pet", 0, 0.0, "Pet");
+   }
 
+   //MUTATOR
+   //setName mutator
+   // sets the name of pet
+   public void setName(String name) {
+      this.name = name;
+   }
+   
+   //setAge mutator
+   // sets the age of pet
+   public void setAge(int age) {
+      newAge = age;
+   }
+   
+   //setWeight mutator 
+   //sets the weight of pet
+   public void setWeight(double weight) {
+      newWeight = weight;
+   }
+   
+   //setAnimal mutator 
+   //sets the type of animal of the pet
+   public void setAnimal(String typeOfAnimal) {
+      newTypeOfAnimal = typeOfAnimal;
+   }
+   
+   //ACCESSORS
+   // getName accessor
+   // gets name
+   public String getName() {
+      return this.name;
+   }
+   
+   //getAge accessor
+   //gets age
+   public int getAge() {
+      return this.newAge;
+   }
+   
+   //getWeight accessor
+   //gets weight
+   public double getWeight() {
+      return this.newWeight;
+   }
+   
+   //getAnimal accessor
+   //gets type of animal
+   public String getAnimal() {
+      return this.newTypeOfAnimal;
+   }
+   
+   //ToString method 
+   //method that will return template for output
+   public String toString() {
+      return "Pet: name = " + name + " , age = " + newAge + " , weight = " + newWeight + " , type of animal = " + newTypeOfAnimal;
+   }
 }
