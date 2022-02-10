de8e4681525a92ef0ca47293324dd57b0776bf08
darwin <95459719+darwinh03@users.noreply.github.com>
2022-02-10 13:17:59 -0800

    Update AnimalClinic.java

--- AnimalClinic.java
+++ AnimalClinic.java
@@ -7,15 +7,6 @@ Animal clinic class will create pet objects and call all of the pet object metho
 public class AnimalClinic {
    public static void main(String[] args) {
       // create 3 Pet objects, using both of the constructors
-      Pet oscar = new Pet();/* 
-Darwin Hanson
-1/31/22
-
-Animal clinic class will create pet objects and call all of the pet object methods.
-*/
-public class AnimalClinic {
-   public static void main(String[] args) {
-      // create 3 Pet objects, using both of the constructors
       Pet oscar = new Pet();
       Pet phoebe = new Pet();
       Pet karly = new Pet();
@@ -58,15 +49,5 @@ public class AnimalClinic {
       System.out.println(phoebe.toString());
       System.out.println(karly.toString());
       // use toString method to print every pet
-   }
-}
-      Pet phoebe = new Pet();
-      Pet karly = new Pet();
-      // call every accessor and mutator
-      
-      // print out each Pet object
-      System.out.println(oscar.ToString());
-      System.out.println(phoebe.ToString());
-      System.out.println(karly.ToString());
    }
 }
